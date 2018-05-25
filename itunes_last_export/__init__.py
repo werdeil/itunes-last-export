276656233c020c1f8a65fc7aa4d6bf56669fb1ac
werdeil <werdeil@hotmail.com>
2018-05-25 20:09:10 +0200

    Update setup to prepare the deploy to pypi

--- itunes_last_export/__init__.py
+++ itunes_last_export/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+"""An application to export last.fm scrobbles to iTunes"""
+
+name = "itunes_last_export"
+
 # import itunes_last_export.import_loved_tracks
 import itunes_last_export.update_playcount
 import itunes_last_export.db_management
--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+import setuptools
 
-setup(name='itunes_last_export',
-      version='0.1',
-      description='Tool to import playcounts and loved tracks from your last.fm account into iTunes',
-      author='Vincent Verdeil',
-      author_email='vincent.verdeil@gmail.com',
-      url='https://github.com/werdeil/itunes-last-export',
-      scripts=['bin/itunes_last_export'],
-      packages=['itunes_last_export'],
-      package_data={'itunes_last_export': ['*.png']},
-      )
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="itunes_last_export",
+    version="0.0.1",
+    author="Vincent Verdeil",
+    author_email="vincent.verdeil@gmail.com",
+    description="ool to import playcounts and loved tracks from your last.fm account into iTunes",
+    long_description=long_description,
+    url="https://github.com/werdeil/itunes_last_export",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        'Development Status :: 3 - Beta',
+        'Environment :: MacOS X',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Topic :: Multimedia :: Sound/Audio',
+    ),
+)

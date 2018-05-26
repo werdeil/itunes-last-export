820b7f876e8159d7a1c2e25effb25d90926819e1
werdeil <werdeil@hotmail.com>
2018-05-26 21:41:37 +0200

    Update setup for next pypi deploy

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="itunes_last_export",
-    version="0.0.1",
+    name="itunes-last-export",
+    version="0.0.3",
     author="Vincent Verdeil",
     author_email="vincent.verdeil@gmail.com",
-    description="ool to import playcounts and loved tracks from your last.fm account into iTunes",
+    description="Tool to import playcounts and loved tracks from your last.fm account into iTunes",
     long_description=long_description,
-    url="https://github.com/werdeil/itunes_last_export",
+    url="https://github.com/werdeil/itunes-last-export",
     packages=setuptools.find_packages(),
     classifiers=(
-        'Development Status :: 3 - Beta',
+        'Development Status :: 4 - Beta',
         'Environment :: MacOS X',
         'Intended Audience :: Developers',
         'Intended Audience :: End Users/Desktop',

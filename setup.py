2a33a4f67c5f1e78b97552e7b55a2465b9d65d1d
werdeil <werdeil@hotmail.com>
2018-05-27 18:47:11 +0200

    Remove bin folder and update setup.py to point to the gui main() method

--- setup.py
+++ setup.py
@@ -8,13 +8,18 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="itunes-last-export",
-    version="0.0.3",
+    version="0.0.4",
     author="Vincent Verdeil",
     author_email="vincent.verdeil@gmail.com",
     description="Tool to import playcounts and loved tracks from your last.fm account into iTunes",
     long_description=long_description,
     url="https://github.com/werdeil/itunes-last-export",
-    packages=setuptools.find_packages(),
+    license='GPLv3',
+    keywords=[
+        'iTunes',
+        'Last.fm',
+        'scrobble'
+    ],
     classifiers=(
         'Development Status :: 4 - Beta',
         'Environment :: MacOS X',
@@ -24,4 +29,13 @@ setuptools.setup(
         'Programming Language :: Python',
         'Topic :: Multimedia :: Sound/Audio',
     ),
+    packages=setuptools.find_packages(),
+    package_data={
+        'itunes_last_export.images': ['*.png'],
+    },
+    include_package_data=True,
+    install_requires=[
+        'requests'
+    ],
+    entry_points={'console_scripts': ["itunes-last-export = itunes_last_export.gui:main"]},
 )
